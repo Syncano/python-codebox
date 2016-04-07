64d43c36f18ca9d10af3cf4f0556d1e82c7b55e4
Daniel Kopka <daniel.kopka@syncano.com>
2016-04-07 11:47:51 +0200

    assert fix

--- test_v50.py
+++ test_v50.py
@@ -1,2 +1,2 @@
-for lib, version in (('numpy', '1.8.2'), ('scipy', '0.13.3'), ('syncano', '5.0.0')):
+for lib, version in (('numpy', '1.8.2'), ('scipy', '0.13.3'), ('syncano', '5.0.1')):
     assert __import__(lib).__version__ == version
