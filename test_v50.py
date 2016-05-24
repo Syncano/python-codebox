9b250ff621736c696df4f25ec918e0001398954a
Sebastian Opałczyński <opalczynski@gmail.com>
2016-05-24 11:00:09 +0200

    [CORE-1518] update the test with new LIB version;

--- test_v50.py
+++ test_v50.py
@@ -1,2 +1,2 @@
-for lib, version in (('numpy', '1.8.2'), ('scipy', '0.13.3'), ('syncano', '5.0.1')):
+for lib, version in (('numpy', '1.8.2'), ('scipy', '0.13.3'), ('syncano', '5.1.0')):
     assert __import__(lib).__version__ == version
