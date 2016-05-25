9a2cbbd3cc06e570d644c8096dbae407731f7d81
Zhe Brah <zhebrah@gmail.com>
2016-05-25 11:12:53 +0200

    [INFRA-294]: temporary fix

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
     import pkgutil
     modules = (name for _, name, is_pkg in pkgutil.iter_modules()
-               if is_pkg)
+               if is_pkg and name != 'aexol')
     map(__import__, modules)
