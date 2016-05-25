0dfbb3f7f7d6115a89227852dcb27f6e3ea3bc50
Zhe Brah <zhebrah@gmail.com>
2016-05-25 13:38:00 +0200

    [INFRA-294]: revert

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
     import pkgutil
     modules = (name for _, name, is_pkg in pkgutil.iter_modules()
-               if is_pkg and name != 'aexol')
+               if is_pkg)
     map(__import__, modules)
